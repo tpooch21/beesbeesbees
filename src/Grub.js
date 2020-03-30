9600206f51216a4196a2a6fd03487089d4a5e246
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-30 11:35:40 -0700

    Add unique properties and shared methods to Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,20 @@
+// Create a Grub Class, serves as the superclass for all related bee subclasses
+// Superclass and subclasses to utilize pseudoclassical instantiation patterns
+// Should not require an input value
+
+// Grub will include the following differentiating properties:
+//  Age: set to zero
+//  Color: set to pink
+//  Food: set to jelly
+//  Eat method: Defined on Grub constructor prototype since it's considered instance similarity code
+
 var Grub = function() {
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
 };
+
+Grub.prototype.eat = function() {};
 
