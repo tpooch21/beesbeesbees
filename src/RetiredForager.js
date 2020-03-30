9cd2a599ace9e1cd0f82836a08e238ecf31b441d
Trevor Puccini <trevorpuccini21@gmail.com>
2020-03-30 12:33:48 -0700

    Add properties and methods to RetiredForagerBee subclass

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,37 @@
+// RetiredForagerBee is a subclass of ForagerBee
+
+// Properties
+// Age: 40 (update)
+// Job: 'gamble' (update)
+// canFly: false (update)
+// Color: grey (update)
+// Food: inherited from Grub via ForagerBee via Bee
+// treasureChest: inherited from ForagerBee
+
+// Methods
+// Forage: returns 'I am too old, let me play cards instead' (update)
+// Eat: inherited from Grub via ForagerBee via Bee
+// Gamble: Adds treasure to treasureChest
+
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+
 };
+
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+};
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
 
